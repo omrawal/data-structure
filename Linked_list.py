0f62c9dc2292e967321c84107e3482dda9ff2f16
Om Rawal <omrawal007@gmail.com>
2020-09-23 09:10:04 +0530

    addend completed

---
+++ Linked_list.py
@@ -0,0 +1,34 @@
+# singly linked list
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedlist:
+    def __init__(self):
+        self.head=None
+    def addend(self,data):
+        temp=self.head
+        newNode=node(data)
+        if(temp==None):
+            self.head=newNode
+        else:
+            while(temp.next!=None):
+                temp=temp.next
+            temp.next=newNode
+    def disp(self):
+        temp=self.head
+        while(temp!=None):
+            print(temp.data,end=" ")
+            temp=temp.next
+        print()
+ll=linkedlist()
+ll.addend(5)
+ll.addend(6)
+ll.disp()
+ll.addend(55)
+ll.addend(66)
+ll.disp()
+
+
+
